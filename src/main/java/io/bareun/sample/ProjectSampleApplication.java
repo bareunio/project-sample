37090087684a66911ec6e76305ee0733dd5bdb20
discphy <dinophy@nate.com>
2024-07-05 10:00:33 +0900

    feat(add) : 바른아이오 프레임워크 Component scan 설정

--- src/main/java/io/bareun/sample/ProjectSampleApplication.java
+++ src/main/java/io/bareun/sample/ProjectSampleApplication.java
@@ -2,7 +2,9 @@ package io.bareun.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"io.bareun"})
 @SpringBootApplication
 public class ProjectSampleApplication {
 
